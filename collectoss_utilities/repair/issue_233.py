677db21e92d77445b8ea356c6c3df6e19399a1af
Adrian Edwards <adredwar@redhat.com>
2026-03-18 12:19:43 -0400

    add selfcheck report command and the first query
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss_utilities/repair/issue_233.py
+++ collectoss_utilities/repair/issue_233.py
@@ -9,6 +9,7 @@ from collectoss.application.cli import (
     with_database,
     DatabaseContext,
 )
+from augur.application.db.models.augur_data import Commit
 
 # from collectoss.application.db.session import DatabaseSession
 from datetime import datetime
@@ -25,3 +26,23 @@ logger = logging.getLogger(__name__)
 def cli(ctx):
     ctx.obj = DatabaseContext()
 
+
+@cli.command("report")
+@test_connection
+@test_db_connection
+@with_database
+@click.pass_context
+def run_selftest_report(ctx):
+    """
+    Run queries to evaluate various aspects of the augur system's functioning and produce a report
+    """
+    cmt_author_name_issue_3740_query = (
+        select(func.count())
+        .select_from(Commit)
+        .where(Commit.cmt_author_name.is_(''))
+    )
+    cmt_author_name_issue_3740_count = None
+
+    with ctx.obj.engine.begin() as connection:
+        cmt_author_name_issue_3740_count = connection.execute(cmt_author_name_issue_3740_query).one()
+        click.echo(f'Issue 3740 count: {cmt_author_name_issue_3740_count}')
