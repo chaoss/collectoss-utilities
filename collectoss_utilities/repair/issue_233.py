50d2fb2718a24d467a9ea844af4a6e1b7e100dac
Adrian Edwards <adredwar@redhat.com>
2026-03-18 14:39:28 -0400

    fixups
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss_utilities/repair/issue_233.py
+++ collectoss_utilities/repair/issue_233.py
@@ -2,6 +2,7 @@
 import logging
 import click
 import sqlalchemy as s
+from sqlalchemy import select, func
 
 from collectoss.application.cli import (
     test_connection,
@@ -36,13 +37,15 @@ def run_selftest_report(ctx):
     """
     Run queries to evaluate various aspects of the augur system's functioning and produce a report
     """
+    click.echo('Generating Augur selftest report....')
+
     cmt_author_name_issue_3740_query = (
         select(func.count())
         .select_from(Commit)
-        .where(Commit.cmt_author_name.is_(''))
+        .where(Commit.cmt_author_name == '')
     )
     cmt_author_name_issue_3740_count = None
 
     with ctx.obj.engine.begin() as connection:
-        cmt_author_name_issue_3740_count = connection.execute(cmt_author_name_issue_3740_query).one()
-        click.echo(f'Issue 3740 count: {cmt_author_name_issue_3740_count}')
+        cmt_author_name_issue_3740_count = connection.execute(cmt_author_name_issue_3740_query).scalar_one()
+        click.echo(f'Issue 3740 count: {cmt_author_name_issue_3740_count} commit files in the `commits` table contain authors with an empty string as their name')
