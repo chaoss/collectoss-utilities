1da35a49735d1138551cfcea9d94c5ea47644feb
Adrian Edwards <adredwar@redhat.com>
2026-03-25 09:19:21 -0400

    selfcheck repar via cli (start)
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss_utilities/repair/issue_233.py
+++ collectoss_utilities/repair/issue_233.py
@@ -2,7 +2,13 @@
 import logging
 import click
 import sqlalchemy as s
+import csv
 from sqlalchemy import select, func
+from augur.application.db.lib import get_repo_by_repo_id
+from augur.application.db.session import DatabaseSession
+
+from augur.application.config import AugurConfig
+from augur.tasks.git.util.facade_worker.facade_worker.utilitymethods import get_absolute_repo_path
 
 from collectoss.application.cli import (
     test_connection,
@@ -17,6 +23,10 @@ from datetime import datetime
 from collectoss.application.db.models import Repo
 
 from ._cli_util import get_db_version
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 logger = logging.getLogger(__name__)
@@ -49,3 +59,75 @@ def run_selftest_report(ctx):
     with ctx.obj.engine.begin() as connection:
         cmt_author_name_issue_3740_count = connection.execute(cmt_author_name_issue_3740_query).scalar_one()
         click.echo(f'Issue 3740 count: {cmt_author_name_issue_3740_count} commit files in the `commits` table contain authors with an empty string as their name')
+
+
+@cli.command("repair")
+@click.option("--batch-size", default=1000, help="Set the number of records to repair in each repair operation (to avoid queries taking forever)")
+@click.option("--dry-run", is_flag=True, default=False, help="Skip the final updating of values to demonstrate what work would be done without doing it")
+@test_connection
+@test_db_connection
+@with_database
+@click.pass_context
+def run_selftest_repair(ctx, batch_size, dry_run):
+
+    tool_source = "Augur Selftest Repair"
+    tool_version = "0.1"
+
+    click.echo("Checking for data corrections to perform")
+    
+    click.echo("Checking for missing commit author names...")
+
+    # This checker for missing commit author names is a necessary fixup for https://github.com/chaoss/augur/issues/3740
+    # it is written as a series of queries to read data esssentially field-by-field to narrow the results down because
+    # the commits table actually stores commit files (https://github.com/chaoss/augur/issues/3682)
+    # the structure of this tool is also intended to output a list of confirmed affected records first,
+    # so that detailed records can be kept by the augur admin if desired.
+
+    with DatabaseSession(logger, ctx.obj.engine) as session:
+        config = AugurConfig(logger, session)
+        
+        repo_base_directory = config.get_value("Facade", "repo_directory")
+        if repo_base_directory is None:
+            raise ValueError("Augur should have a facade repo base directory set in the config. It is unsafe to continue without one")
+
+
+    with ctx.obj.engine.begin() as connection:
+        # any queries that attempt to get one row per commit are incredibly slow
+        query = s.select(func.distinct(Commit.repo_id)).where(Commit.cmt_author_name == '').limit(20)
+        repos = connection.execute(query).scalars().all()
+    
+        # click.echo("\tProcessing empty commit authors")
+
+        for repo_id in repos:
+
+            repo = get_repo_by_repo_id(repo_id)
+         
+            #Get the huge list of commits to process.
+            absolute_path = get_absolute_repo_path(repo_base_directory, repo.repo_id, repo.repo_path, repo.repo_name)
+            repo_loc = (f"{absolute_path}/.git")
+
+            click.echo(f"Checking repo id {repo_id}, path {absolute_path}")
+            query = s.select(func.distinct(Commit.cmt_commit_hash)).where(Commit.cmt_author_name == '', Commit.repo_id == repo_id)
+            unique_commit_hashes = connection.execute(query).scalars().all()
+            click.echo(len(unique_commit_hashes))
+              # TODO: save/append to list of affected commits
+              
+            # for commit in unique_commit_hashes:
+            #     click.echo(commit)
+
+            # click.echo(commit_entries)
+          
+
+
+    
+
+    if not dry_run:
+        with ctx.obj.engine.begin() as connection:
+            pass
+        
+        # TODO, update tool_source and tool_version and collection date.
+
+        # # any queries that attempt to get one row per commit are incredibly slow
+        # query = s.select(Commit.repo_id).distinct().where(Commit.cmt_author_name.is_(None)).limit(20)
+        
+        # result = connection.execute(query).all()
