d9044e667a1e27492eed6a013413df2a67c6c24f
Adrian Edwards <adredwar@redhat.com>
2026-05-01 19:43:43 -0400

    hello world CLI structure
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

---
+++ collectoss_utilities/cli/main.py
@@ -0,0 +1,10 @@
+import click
+
+@click.command()
+def run():
+    """
+    CollectOSS-utilities is an application for open source community health analytics
+    """
+    click.echo("Hello from collectoss-utilities!")
+
+
